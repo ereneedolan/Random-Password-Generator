52215557be9711242374dea7c795a599f56fa5be
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-09-29 17:18:24 -0400

    developed varaibles and boolean factors for user to answer

--- script.js
+++ script.js
@@ -1,38 +1,76 @@
 // Assignment code here
 
-  
+// var generateBtn = document.querySelectorAll('#generate');
 
-var generateBtn = document.querySelectorAll('#generate');
 
-  
 
-function generatePassword() {}
+function generatePassword() {
 
-//Data
+  //Data
 
-//we need different sets of characters (use variables) - maybe strings or arrays, string give us the benefit of using indices to target things at different positions
-//number
-//ordered list to store use arrays and strings for indices and randomly select things
-//lowercase character
-// uppercase characters
-// special characters
-// number of character
-// whether they chose numbers, lowercase, uppercase or special characters
+  //we need different sets of characters (use variables) - maybe strings or arrays, string give us the benefit of using indices to target things at different positions
+  //number
+  //ordered list to store use arrays and strings for indices and randomly select things
+  //lowercase character
+  // uppercase characters
+  // special characters
+  // number of character
+  // whether they chose numbers, lowercase, uppercase or special characters
 
-  
 
-//how many characters do you want to use? some number
+
+  //how many characters do you want to use? some number
   // do you want to use numbers? yes or no
   // do you want to use lowercase characters? yes or no
   // do you want to use uppercase characters? yes or no
   // do you want to use special characters? yes or no
 
+  var uppercase = "ABCD"
+  var uppercaseResponse = confirm("Do you want to use uppercase letters in your password?")
 
-// randomly select characters from all of the selected characters
-// put them together
+  var lowercase = "abcd"
+  var lowercaseResponse = confirm("Do you want to use lowercase letters in your password?")
+
+  var specialChacters = " !#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\""
+  var specialCharactersResponse = confirm("Do you want to use special characters in your passwor?")
+
+  var number = "1234567890"
+  var numberResponse = confirm("Do you want to use numbers in your password?")
+
+  var charactersForPassword = ""
+
+  if (uppercaseResponse === true) {
+    charactersForPassword = uppercase + charactersForPassword
+  }
+  if (lowercaseResponse === true) {
+    charactersForPassword = lowercase + charactersForPassword
+  }
+  if (specialCharactersResponse === true) {
+    charactersForPassword = specialChacters + charactersForPassword
+  }
+  if (numberResponse === true) {
+    charactersForPassword = number + charactersForPassword
+  }
+
+  var lengthOfPassword = prompt("Please choose a password length between 8 and 128 characters")
+  if (!(lengthOfPassword >= 8) && !(lengthOfPassword <= 128)) {
+    alert("Your password needs to be between 8-128 characters long")
+    return
+  }
 
   
-return "password"
+var finishedPassword = ""
+// get a random number generated from math random, math floor and then put in loop, add results from charactersForPasswordcharat to variable finished password then return that
+charactersForPassword.charAt()
+
+
+
+  // randomly select characters from all of the selected characters
+  // put them together
+
+
+  return finishedPassword
+}
 
 var generateBtn = document.querySelector("#generate");
 
